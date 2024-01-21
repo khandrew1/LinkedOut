eeb5275ff797407f5731658e8f8821aae59f71d5
Andrew Khadder <andrewkh15@gmail.com>
2024-01-20 18:44:10 -0800

    add next auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
+"use client"
+import { signIn } from "next-auth/react";
+
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-full overflow-hidden">
       <p className="text-9xl">Linked<span className="line-through">Out</span></p>
       <div className="flex gap-3">
-        <button>Login</button>
-        <button>Sign Up</button>
+        <button onClick={() => signIn("google")}>Login</button>
       </div>
     </div>
   );
