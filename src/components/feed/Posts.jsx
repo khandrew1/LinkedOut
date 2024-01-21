d91fe51b1853ed9dd217d0b14370967e3568beed
Andrew Khadder <andrewkh15@gmail.com>
2024-01-20 23:45:12 -0800

    i dont even know

--- src/components/feed/Posts.jsx
+++ src/components/feed/Posts.jsx
@@ -2,17 +2,35 @@
 
 import { useSession } from "next-auth/react";
 import Post from "./Post";
+import Modal from "@/components/Modal";
+import { useState } from "react";
 
 const Posts = () => {
   const { data: session } = useSession();
+  const [modal, setModal] = useState(false);
 
   return (
-    <Post
-      name={session.user.name}
-      picture={session.user.image}
-      time="1w"
-      message="I'm happy to share that I'm no longer friends with David Chong!"
-    />
+    <div className="h-full flex flex-col items-center gap-3">
+      {modal && (
+        <Modal
+          name={session.user.name}
+          pfp={session.user.image}
+          setModal={setModal}
+        />
+      )}
+      <button
+        className="rounded-full bg-blue-200 w-20 h-12 font-bold"
+        onClick={() => setModal(true)}
+      >
+        Post
+      </button>
+      <Post
+        name={session.user.name}
+        picture={session.user.image}
+        time="1w"
+        message="I'm happy to share that I'm no longer friends with David Chong!"
+      />
+    </div>
   );
 };
 
