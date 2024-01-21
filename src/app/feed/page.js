57f2164036b7e33feb481092cf3b34a09959fcd8
Andrew Khadder <andrewkh15@gmail.com>
2024-01-20 19:16:22 -0800

    added session provider

--- src/app/feed/page.js
+++ src/app/feed/page.js
@@ -1,10 +1,17 @@
+"use client";
+
 import Post from "@/components/Post";
+import { useSession, signOut } from "next-auth/react";
 
 const Page = () => {
+
+  const { data: session } = useSession();
+
   return (
-    <div className="h-full flex flex-col justify-center items-center">
+    <div className="h-full flex flex-col justify-center items-center gap-3">
       <Post />
       <Post />
+      <button onClick={() => signOut()}>log</button>
     </div>
   );
 };
