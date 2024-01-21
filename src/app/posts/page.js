d454f0684b257f7a393c3693a3f764d2175475df
Andrew Khadder <andrewkh15@gmail.com>
2024-01-20 16:03:15 -0800

    added post component

---
+++ src/app/posts/page.js
@@ -0,0 +1,11 @@
+import Post from "@/components/Post";
+
+const Page = () => {
+  return (
+    <div className="h-full flex justify-center items-center">
+      <Post />
+    </div>
+  );
+};
+
+export default Page;
